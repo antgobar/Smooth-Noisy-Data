9b81abf36a4dcba92b074f335546ef3fd4c63323
AGB09 <60256070+AGB09@users.noreply.github.com>
2020-01-31 11:04:35 +0000

    Update average_smoothing_XRD.py

--- average_smoothing_XRD.py
+++ average_smoothing_XRD.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Jan 23 10:28:41 2020
 
-@author: AGB90
+@author: preagoe
 
 Take .csv data and perform moving centered average over range x
 """
@@ -16,20 +16,16 @@ import csv
 #import math
 
 #set directory
-os.chdir('path')
+os.chdir('C:\Users\preagoe\OneDrive - University of Leeds\Python_programing\Python2')
 
 #file to smooth
 filename ='noisy_XRD_data.csv'
 
-#data cleaning
+#data cleaning - remove top x rows and bottom y rows
 xrd_data = pd.read_csv(filename, skiprows = 25, skipfooter = 3, engine = 'python') #engine = 'python' when using skipfooter
 
-#print xrd_data.head()
-
-#data cleaning 2
+#data cleaning  - remove middle column
 new_data = xrd_data.drop(xrd_data.columns[[1]], axis = 1)
-
-#print new_data.head()
 
 #select relevant x y cols
 theta_angle = new_data.iloc[:,0]
@@ -52,7 +48,7 @@ for number in theta_angle: #summing 2theta data
     theta_smooth.append(avg_2theta)
     start_no +=1
 
-#reinitialise smoothing parameters - fix this
+#reinitialise smoothing parameters - how to avoid initialising twice?
 av_no = 10
 start_no = 0
     
@@ -66,3 +62,5 @@ plt.plot(theta_smooth, intensity_smooth, color = 'r')
 
 #concatenate lists
 smooth_XRD = zip(theta_smooth, intensity_smooth)
+
+#write out data to .csv ???
